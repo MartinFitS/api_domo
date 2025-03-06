3043dbf144cde32dbb6b6e050eda4a6b74ccfd5f
MartinFitS <sernamartin46@gmail.com>
2025-03-06 17:51:42 -0600

    faces.py new changes

--- routes/faces.py
+++ routes/faces.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from typing import List
+import jwt
+import datetime
+from bson import ObjectId
 from services.faces import recibir_foto, train_model_function,recognize_face
 
 router = APIRouter()
+
+
+SECRET_KEY = "tu_secreto_super_seguro"
+ALGORITHM = "HS256"
 
 class FaceLoginRequest(BaseModel):
     img: str  
@@ -15,6 +22,19 @@ class ImageData(BaseModel):
 class ImageUploadRequest(BaseModel):
     username: str
     images: List[ImageData]
+
+def create_jwt_token(user_data):
+    user_id = str(user_data["_id"]) if "_id" in user_data else None
+    safe_user_data = {k: v for k, v in user_data.items() if not isinstance(v, bytes)}
+
+    payload = {
+        "sub": user_id,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+        "iat": datetime.datetime.utcnow(),
+        "user": safe_user_data
+    }
+
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 @router.post("/save_faces")
 def save_faces(request: ImageUploadRequest):
@@ -46,12 +66,17 @@ async def login_face(request: FaceLoginRequest):
         user_data = recognize_face(request.img)  # Llamamos a la función de reconocimiento
         print(user_data)
 
+           
+        token = create_jwt_token(user_data)
+
         if not user_data:
             raise HTTPException(status_code=401, detail="No se pudo autenticar al usuario")
 
         return {
             "message": "Usuario autenticado exitosamente",
-            "user": user_data
+            "user": user_data,
+            "token": token
+
         }
 
     except HTTPException as e:
